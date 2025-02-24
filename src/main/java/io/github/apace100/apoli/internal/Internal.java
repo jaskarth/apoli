95d639d93333ae9f53d3f57ae1ff8981429fffac
Ampflower 🌺 <git@ampflower.gay>
2025-02-24 11:31:01 -0800

    Fix another memory leak with DefaultAttributeContainer
    Evidently, the EntityAttributeInstances that were present also have owner entities attached.

--- src/main/java/io/github/apace100/apoli/internal/Internal.java
+++ src/main/java/io/github/apace100/apoli/internal/Internal.java
@@ -2,11 +2,14 @@ package io.github.apace100.apoli.internal;
 
 import io.github.apace100.apoli.access.EntityLinkedType;
 import io.github.apace100.apoli.access.OwnableAttributeContainer;
+import io.github.apace100.apoli.mixin.internal.DefaultAttributeContainerAccessor;
 import io.github.apace100.apoli.mixin.internal.DefaultAttributeRegistryAccessor;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.registry.Registries;
 import org.jetbrains.annotations.ApiStatus;
+
+import java.util.Collection;
 
 /**
  * @author Ampflower
@@ -28,9 +31,23 @@ public final class Internal {
         }
 
         for (final DefaultAttributeContainer container : DefaultAttributeRegistryAccessor.apoli$getRegistry().values()) {
-            if (container instanceof OwnableAttributeContainer ownableAttributeContainer) {
-                ownableAttributeContainer.apoli$setOwner(null);
+            clearOwnableAttribute(container);
+
+            if (container instanceof DefaultAttributeContainerAccessor accessor) {
+                clearOwnableAttributes(accessor.getInstances().values());
             }
+        }
+    }
+
+    private static void clearOwnableAttributes(Collection<?> ownableAttributeContainers) {
+        for (final var entry : ownableAttributeContainers) {
+            clearOwnableAttribute(entry);
+        }
+    }
+
+    private static void clearOwnableAttribute(Object ownableAttribute) {
+        if (ownableAttribute instanceof OwnableAttributeContainer ownableAttributeContainer) {
+            ownableAttributeContainer.apoli$setOwner(null);
         }
     }
 }
